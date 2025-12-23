a4108c437c2ad5dc2939db36052c6e43498db1ee
Yadelew-zemene <yadelewzemene@gmail.com>
2025-12-23 12:44:30 +0300

    chore: setup MySQL connection using mysql2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import cors from "cors"
+import pool from './config/db.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/health", (req, res) => {
-    res.status(200).json({status:"ok",message:"Skill match running"})
+    res.status(200).json({ status: "ok", message: "Skill match running" })
+
     
 });
+app.get("/test-db", async (req, res) => {
+        try {
+            const [rows] = await pool.query("SELECT 1")
+            res.json({ success: true, massage: "Database connected" });
+        
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
+    }
+})
 export default app;
